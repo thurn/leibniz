8a0ed628a64ee95ec7dae3d21b1e513400d5b105
Derek Thurn <derek@fake.email>
2013-10-28 18:20:01 -0700

    Initial commit

---
+++ src/ca/thurn/leibniz/Leibniz.java
@@ -0,0 +1,14 @@
+package ca.thurn.leibniz;
+
+import com.firebase.client.Firebase;
+
+import android.util.Log;
+
+public class Leibniz {
+
+  public static void main(String[] args) {
+    Log.e("why", ">>>>> main ");
+    Firebase firebase = new Firebase("firebase");
+  }
+
+}
